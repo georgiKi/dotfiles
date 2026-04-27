8df675307459792513e08611b5e2a68f63b02148
Georgi Kirilov <georgi.kirilov@dustin.com>
2026-04-27 15:32:08 +0200

    fix

--- .config/nvim/lua/plugins/ui/treesitter.lua
+++ .config/nvim/lua/plugins/ui/treesitter.lua
@@ -9,6 +9,8 @@ return {
     require("nvim-ts-autotag").setup()
     require("nvim-treesitter").setup()
     local install = require("nvim-treesitter.install")
+    local parsers = { "c", "c_sharp", "lua", "vim", "regex", "javascript", "typescript", "tsx", "svelte", "css", "scss",
+      "html",
       "json", "markdown", "markdown_inline", "jsdoc" }
     install.install(parsers, { skip_installed = true })
     vim.api.nvim_create_autocmd("FileType", {
